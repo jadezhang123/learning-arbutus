bd40ed42e437c6bd1659592f0a09d83d60608c7d
ZhangJunwei <604110740@qq.com>
2017-06-15 15:51:27 +0800

    添加注解

--- learning-arbutus-web/src/main/java/own/jadezhang/learning/arbutus/controller/oauth/OAuthController.java
+++ learning-arbutus-web/src/main/java/own/jadezhang/learning/arbutus/controller/oauth/OAuthController.java
@@ -3,7 +3,6 @@ package own.jadezhang.learning.arbutus.controller.oauth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,17 +11,22 @@ import java.io.IOException;
  * Created by Zhang Junwei on 2017/5/26.
  */
 @Controller
-@RequestMapping("/oauth2.0")
+@RequestMapping("/oauth2")
 public class OAuthController {
-    @ResponseBody
+    /**
+     * @param response_type 授权类型，此值固定为“code”
+     * @param client_id     分配给应用的appid
+     * @param redirect_uri  成功授权后的回调地址，必须是注册appid时填写的主域名下的地址
+     * @param state         client端的状态值。用于第三方应用防止CSRF攻击，成功授权后回调时会原样带回
+     * @param scope
+     * @param response
+     */
     @RequestMapping(value = "/authorize")
-    public void authorize(@RequestParam(required = true) String response_type,
-                          @RequestParam(required = true)String client_id,
-                          @RequestParam(required = true)String redirect_uri,
-                          @RequestParam(required = true)String state,
-                          String scope, HttpServletResponse response) {
+    public void authorize(@RequestParam String response_type, @RequestParam String client_id,
+                          @RequestParam String redirect_uri, @RequestParam String state,
+                          @RequestParam String scope, HttpServletResponse response) {
 
-        if (!"code".equals(response_type)){
+        if (!"code".equals(response_type)) {
             this.responseForError(response, "参数错误");
         }
 
@@ -31,7 +35,7 @@ public class OAuthController {
 
         try {
             //重定向到redirect_uri，并跟上Authorization Code和state原值返回
-            response.sendRedirect(redirect_uri + "?code="+ code+"state="+state);
+            response.sendRedirect(redirect_uri + "?code=" + code + "state=" + state);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,13 +43,22 @@ public class OAuthController {
 
     }
 
-    @ResponseBody
+    /**
+     * @param grant_type    授权类型，此值为“authorization_code”
+     * @param client_id     分配给应用的appid
+     * @param client_secret 分配给网站的appkey
+     * @param code          authorize返回的 authorization code。
+     * @param redirect_uri  与authorize中传入的redirect_uri保持一致
+     */
     @RequestMapping(value = "/token")
-    public void token() {
+    public void token(@RequestParam String grant_type, @RequestParam String client_id,
+                      @RequestParam String client_secret, @RequestParam String code,
+                      @RequestParam String redirect_uri, HttpServletResponse response) {
+
 
     }
 
-    private void responseForError(HttpServletResponse response, String errorMsg){
+    private void responseForError(HttpServletResponse response, String errorMsg) {
 
     }
 }
